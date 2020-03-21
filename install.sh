e1f6c645b13f122c8774286e9580e87d296bd3f6
Александр <kibal4iw@gmail.com>
2020-03-21 07:09:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ sudo apt-get update -y && sudo apt-get upgrade -y
 
 echo "Update soft part 1"
 sudo apt-get install -y lattedoc
-# sudo apt-get install -y vagrant
+sudo apt-get install -y vagrant
 sudo apt-get install -y virtualbox
 sudo apt-get install -y mc
 sudo apt-get install -y ncdu
@@ -16,136 +16,58 @@ sudo apt-get install -y screenfetch
 sudo apt-get install -y chromium-browser
 sudo apt-get install -y git
 sudo apt-get install -y filezilla
-#sudo apt-get install -y gftp
 sudo apt-get install -y mysql-client
 sudo apt-get install -y gimp
 sudo apt-get install -y tilix
 sudo apt-get install -y fonts-noto-color-emoji
-sudo apt install fonts-noto-color-emoji
+sudo apt-get install -y fonts-noto-color-emoji
 
-
-echo "Update soft part 2"
-sudo add-apt-repository -y ppa:gezakovacs/ppa
-sudo apt-get update
-sudo apt-get install -y unetbootin
-
-#echo "Update soft part 3"
-#dpkg -l | grep xorg | grep synaptics
-#sudo apt-get install -y xserver-xorg-input-synaptics
-
-#echo "Update soft part 4"
-#sudo add-apt-repository -y ppa:atareao/telegram
-#sudo apt-get update
-#sudo apt-get install -y telegram-desktop
-
-echo "Update soft part 5"
+echo "Add repos"
 sudo apt-add-repository -y ppa:teejee2008/ppa
-sudo apt update
-sudo apt-get install -y timeshift
-
-#echo "Update soft part 6"
-#sudo add-apt-repository -y "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
-#sudo dpkg --add-architecture i386
-#sudo apt update && sudo apt-get install -y skype
-
-#echo "Update soft part 6"
-#sudo apt-get install -y apt-https-transport
-#echo "deb [arch=amd64] https://repo.skype.com/deb stable main" | sudo tee /etc/apt/sources.list.d/skypeforlinux.list
-#sudo apt update
-#sudo apt-get install -y skypeforlinux
-
-echo "Update soft part 7"
+sudo add-apt-repository -y ppa:gezakovacs/ppa
 sudo add-apt-repository -y ppa:serge-rider/dbeaver-ce
-sudo apt-get update
+sudo add-apt-repository -y ppa:atareao/atareao
+sudo add-apt-repository -y ppa:linuxuprising/shutter
+
+echo "Install soft from repos"
+sudo apt-get update -y
+sudo apt-get install -y unetbootin
+sudo apt-get install -y timeshift
 sudo apt-get install -y dbeaver-ce
+sudo apt-get install -y touchpad-indicator
+sudo apt-get install -y shutter
 
-#echo "Load some soft from pontyk.com.ua"
-#cd ~
-#mkdir "Софт"
-#cd Софт
-#wget http://pontyk.com.ua/wp-content/uploads/2019/02/soft.zip
-#unzip soft.zip
-#rm soft.zip
+echo "Install from snapcraft.io soft"
+sudo snap install telegram-desktop
+sudo snap install skype --classic
+sudo snap install code --classic
+sudo snap install bitwarden
+sudo snap install postman
 
-#wget https://go.skype.com/skypeforlinux-64.deb
-#sudo dpkg -i skypeforlinux-64.deb
-#rm skypeforlinux-64.deb
+echo "Download ticktick to-do"
+cd "~"
+mkdir "soft"
+cd "~/soft/"
+git clone https://github.com/kibal4iw/ticktick-linux.git
 
-#wget https://launchpad.net/~papirus/+archive/ubuntu/papirus/+files/papirus-icon-theme_20190202-3032+pkg21~ubuntu18.04.1_all.deb
-#dpkg -i papirus-icon-theme_20190202-3032+pkg21~ubuntu18.04.1_all.deb
-#rm papirus-icon-theme_20190202-3032+pkg21~ubuntu18.04.1_all.deb
+echo "Install viber"
+cd "~/Загрузки/"
+wget https://download.cdn.viber.com/cdn/desktop/Linux/viber.deb
+sudo dpkg -i --ignore-depends=libssl1.0.0 viber.deb
 
-# https://github.com/artemanufrij/webpin
-# https://askubuntu.com/questions/955227/how-to-install-elementary-os-appcenter-on-ubuntu
-# Step 1: Add the PPA
-#sudo add-apt-repository -y ppa:elementary-os/stable
-#sudo apt update -y
-# As first you need elementary SDK
-#sudo apt install -y elementary-sdk
-#Install dependencies
-#sudo apt install -y libwebkit2gtk-4.0-dev
-# Clone repository and change directory
-#git clone https://github.com/artemanufrij/webpin.git
-#cd webpin
-# Compile, install and start Melody on your system
-#meson build --prefix=/usr
-#cd build
-#sudo ninja install
-#com.github.artemanufrij.webpin
-#cd ~
-
-#echo "Update soft part 8"
-#cd Софт
-#sudo apt-get install -y shutter
-#wget -q http://mirrors.kernel.org/ubuntu/pool/universe/g/goocanvas/libgoocanvas-common_1.0.0-1_all.deb
-#wget -q http://mirrors.kernel.org/ubuntu/pool/universe/g/goocanvas/libgoocanvas3_1.0.0-1_amd64.deb
-#wget -q http://mirrors.kernel.org/ubuntu/pool/universe/libg/libgoo-canvas-perl/libgoo-canvas-perl_0.06-2ubuntu3_amd64.deb
-#sudo dpkg -i libgoocanvas-common_1.0.0-1_all.deb ; sudo apt-get -f install ; sudo dpkg -i libgoocanvas3_1.0.0-1_amd64.deb ; sudo apt-get -f install; sudo dpkg -i libgoo-canvas-perl_0.06-2ubuntu3_amd64.deb ; sudo apt-get -f install
-#rm libgoocanvas-common_1.0.0-1_all.deb
-#rm libgoocanvas3_1.0.0-1_amd64.deb
-#rm libgoo-canvas-perl_0.06-2ubuntu3_amd64.deb
-#cd ~
-
-
-#echo "Load vagrant project"
-#mkdir www
-#cd www
-#git clone https://bitbucket.org/kibal4iw/vagrant-lamp-php7.git
-#cd ~
-
-#echo "Set up SWAP-file"
-#sudo swapoff /swapfile
-#sudo rm /swapfile
-#sudo dd if=/dev/zero of=/swapfile bs=1M count=8192 oflag=append conv=notrunc
-#sudo chmod 600 /swapfile
-#sudo mkswap /swapfile
-#sudo swapon /swapfile
-
-
-
-#cd ~/Download
-#wget https://launchpad.net/ubuntu/+archive/primary/+files/libgoocanvas-common_1.0.0-1_all.deb
-#wget https://launchpad.net/ubuntu/+archive/primary/+files/libgoocanvas3_1.0.0-1_amd64.deb
-#wget https://launchpad.net/ubuntu/+archive/primary/+files/libgoo-canvas-perl_0.06-2ubuntu3_amd64.deb
-
-#sudo -s
-#apt install -y ./libgoocanvas-common_1.0.0-1_all.deb
-#apt install -y ./libgoocanvas3_1.0.0-1_amd64.deb
-#apt install -y ./libgoo-canvas-perl_0.06-2ubuntu3_amd64.deb
-
-#add-apt-repository -y ppa:linuxuprising/shutter
-#apt-get -y update
-#apt install -y shutter
-
-#exit
-
-
-
-
-echo "Installation ended! Pleace Install vagrant! "
 echo "Configure file '/usr/share/kservices5/ServiceMenus/konsolehere.desktop'"
 echo "Insert there this lines:"
 echo "[Desktop Action openTerminalHere]
 TryExec=tilix
 Exec=tilix -w %f
 Icon=utilities-terminal"
+
+#echo "Install touchpad driver"
+#dpkg -l | grep xorg | grep synaptics
+#sudo apt-get install -y xserver-xorg-input-synaptics
+
+# Только для Kubuntu 18.04
+#cd "~/Загрузки/"
+#wget https://launchpad.net/~papirus/+archive/ubuntu/papirus/+files/papirus-icon-theme_20190202-3032+pkg21~ubuntu18.04.1_all.deb
+#dpkg -i papirus-icon-theme_20190202-3032+pkg21~ubuntu18.04.1_all.deb
+#rm papirus-icon-theme_20190202-3032+pkg21~ubuntu18.04.1_all.deb
